8440bdffdcddfa3ce601e3ab7e5c19d2f62e6312
DianaBerte <dianaberte.go@gmail.com>
2022-11-28 16:55:35 +0100

    Creating index.js and index.html

---
+++ index.js
@@ -0,0 +1,21 @@
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolks = 0.6;
+let pecorino = 50;
+let pepper = 4;
+
+let slicedGuanciale = strips;
+let bowl1;
+bowl1 += eggYolks + pecorino;
+
+let pan;
+let smokedPepper;
+let someSmokedPepper;
+let bowl2;
+bowl2 += bowl1 + someSmokedPepper;
+
+let strips = brownedStrips;
+let brownedStrips = crispedStrips;
+let grease;
+pan += grease
+
